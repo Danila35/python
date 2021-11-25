6319d9b2b1187f8ff76fabc0016a23536d5c7070
Danila35 <73892370+Danila35@users.noreply.github.com>
2021-11-25 13:54:24 +0300

    Update dz2-2.py

--- dz2-2.py
+++ dz2-2.py
@@ -1,17 +1,22 @@
 import json
 
 
-def write_order_to_json(item, quantity, price, buyer, bdate):
-    myorder = {}
-    myorder["item"] = item
-    myorder["quantity"] = quantity
-    myorder["price"] = price
-    myorder["buyer"] = buyer
-    myorder["bdate"] = bdate
-    print(myorder)
-    with open('order.json', 'w') as f_n:
-        json.dump(myorder, f_n, skipkeys=True, indent=4)
-    return 0
-
-
-write_order_to_json('book', '1', '1500 RUB', 'Anton', '2018-12-22')
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open('orders.json') as f_n:
+        f_n_content = f_n.read()
+        obj = json.loads(f_n_content)
+    if obj and 'orders' in obj.keys():
+        old_orders = obj['orders']
+    else:
+        old_orders = []
+    new_order = {
+        'item': item,
+        'quantity': quantity,
+        'price': price,
+        'buyer': buyer,
+        'date': date,
+    }
+    old_orders.append(new_order)
+    dict_to_json = {'orders': old_orders}
+    with open('orders.json', 'w') as f_n:
+        f_n.write(json.dumps(dict_to_json, indent=4))
